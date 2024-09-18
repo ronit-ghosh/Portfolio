34b98fc4399992cf1f912809656b262a7a31ad03
ronit-ghosh <ronitghosh06@gmail.com>
2024-09-18 21:42:14 +0530

    Project & Skill Added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -6,7 +6,7 @@ const ProjectCard = (props) => {
   return (
     <>{
       isLeft ?
-        <section key={title} className="text-subtext body-font">
+        <section key={title} className="text-subtext body-font mt-6">
           <div className="container mx-auto flex md:px-16 px-6 pb-20 md:flex-row flex-col items-center">
             <div className="lg:max-w-lg lg:w-full md:w-1/2 w-full mb-10 md:mb-0">
               <img className="object-cover object-fit rounded bg-no-repeat" alt={title} src={image} />
@@ -25,9 +25,10 @@ const ProjectCard = (props) => {
               </div>
             </div>
           </div>
+          <div class="p-[.5px] bg-gradient-to-r from-transparent via-[#4c4c4c] to-transparent rounded-lg"></div>
         </section>
         :
-        <section key={title} className="text-subtext body-font">
+        <section key={title} className="text-subtext body-font mt-6">
           <div className="container mx-auto flex md:px-16 px-6 pb-20 md:flex-row flex-col items-center">
             <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-heading">{title}</h1>
@@ -46,6 +47,7 @@ const ProjectCard = (props) => {
               <img className="object-cover object-fit rounded bg-no-repeat" alt={title} src={image} />
             </div>
           </div>
+          <div class="p-[.5px] bg-gradient-to-r from-transparent via-[#4c4c4c] to-transparent rounded-lg"></div>
         </section>
     }
     </>
