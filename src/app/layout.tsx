6a356933dc2fea99d65b258f3f4f76923bb49fb3
ronit-ghosh <ronitghosh06@gmail.com>
2025-03-08 11:50:02 +0530

    GTM Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
+        <GoogleTagManager gtmId="G-L61367LHG7" />
+        <GoogleAnalytics gaId="G-L61367LHG7" />
         {children}
       </body>
     </html>
